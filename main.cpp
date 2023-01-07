dd3948685fe3567b188230e0aa3c09d39b66ddd1
cs-wo-benkyousuru <ma_likai@icloud.com>
2023-01-07 11:55:28 +0800

    remove something

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 #include "Solution.h"
 #include "Dataset.h"
 
-/// @brief 主函数哈哈哈哈哈哈哈
-/// @param argc 命令行参数的个数，没什么用
-/// @param argv 命令行参数，没什么用
-/// @return 返回给操作系统，要不是0就看看代码哪里crash了
 int main(int argc, char** argv)
 {
     for (auto& x : datasets)
